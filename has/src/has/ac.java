ff9f0b4b85f0f6c2440975aaba231f610b2792c7
ashay84 <Ashay@Ashay-PC>
2017-06-08 11:32:46 +0530

    This is class having definations

--- has/src/has/ac.java
+++ has/src/has/ac.java
@@ -1,5 +1,19 @@
 package has;
 
 public class ac extends device {
-
+	
+	protected String name;
+	protected int temp;
+	
+	public void increasetemp()
+	{
+		this.temp=temp++;
+		
+	}
+	
+	public void decreasetemp()
+	{
+		this.temp=temp--;
+		
+	}
 }
